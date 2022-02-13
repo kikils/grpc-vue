0894fcfbf0f7b0447cd2b5af94e786fcf3dc693b
Kazuki Okamoto <kiki.com.start@gmail.com>
2022-02-14 01:06:51 +0900

    Front: 対戦機能

--- frontend/app/src/App.vue
+++ frontend/app/src/App.vue
@@ -1,20 +1,31 @@
 <template>
   <div id='app'>
-    <section>
-      <span class='title-text'>gRPC Client</span>
+    <section class="w-50 mx-auto">
+      <span class='title-text'>gRPC Test Game</span>
       <div class='row justify-content-center mt-4'>
-        <input v-model='inputField' v-on:keyup.enter='addNum' class='mr-1' placeholder='Please input Number'>
-        <button @click='addNum' class='btn btn-primary'>Add Num</button>
+        <input v-model='userId' placeholder='名無し'>
+        <button v-if="!ready" @click="initialize" class='mt-4 btn btn-primary'>
+          ランダムマッチ
+        </button>
+        <button v-else v-bind:disabled="count !== 0 || endCount === 0" @click="countNum" class='mt-4 btn btn-primary'>
+          クリック！
+        </button>
       </div>
     </section>
-    <section>
-      <h2>now total: {{num.total}}</h2>
+    <section class="mt-4 w-70 mx-auto">
+      <h2 v-if="!start && ready">参加者を待っています。。。</h2>
+      <h2 v-if="start && (count > 0)">ゲームスタートまで{{ count }}秒</h2>
+      <h2 v-else-if="count == 0">スタート！！ 残り時間{{ endCount }}秒</h2>
+    </section>
+    <section v-if="count == 0">
+      <h2>あなたのスコア: {{ totalnumsList.filter(el => el.player)[0].total }}</h2>
+      <h2>{{ totalnumsList.filter(el => !el.player)[0].userid }}のスコア: {{ totalnumsList.filter(el => !el.player)[0].total }}</h2>
     </section>
   </div>
 </template>
 
 <script>
-import { addNumParams, getTotalNumParams } from './count_pb'
+import { Null, joinRoomParams, addRoomParams, countNumparams, getRoomTotalNumParams } from './count_pb'
 import { addNumServiceClient } from './count_grpc_web_pb'
 
 export default {
@@ -22,34 +33,116 @@ export default {
   components: {},
   data: function () {
     return {
-      inputField: '',
-      num: 0
+      num: 0,
+      roomId: '',
+      userId: `名無し${Math.floor(Math.random() * 1000)}`,
+      ready: false,
+      start: false,
+      stream: null,
+      totalnumsList: [],
+      count: 10,
+      endCount: 60
     }
   },
   created: function () {
     // eslint-disable-next-line
     this.client = new addNumServiceClient('http://0.0.0.0:9000', null, null)
-    this.getTotalNum()
+  },
+  watch: {
+    start: function(nv) {
+      if (nv) {
+        this.timer(0, 10, () => {this.count -= 1})
+      }
+    },
+    count: function(nv) {
+      if (nv === 0) {
+        this.timer(0, 60, () => {this.endCount -= 1})
+      }
+    }
   },
   methods: {
-    getTotalNum: function () {
-      // eslint-disable-next-line
-      let getRequest = new getTotalNumParams()
-      // eslint-disable-next-line
-      this.client.getTotalNum(getRequest, {}, (err, response) => {
-        this.num = response.toObject()
-        console.log(err, response)
+    initialize: function () {
+      this.getRooms().then(() => this.fetchRoomTotalNums())
+    },
+    timer: function (i, m, f) {
+      if (i < m) {
+        f()
+        setTimeout(function(){this.timer(i+1, m, f)}.bind(this), 1000);
+      }
+    },
+    getRooms: async function () {
+      let getRoomsParam = new Null()
+      await new Promise(resolve => {
+        this.client.getRooms(getRoomsParam, {}, (err, response) => {
+          const rooms = response.toObject()
+          if (rooms.roomsList.find(el => el.isactive) !== undefined) {
+            this.roomId = rooms.roomsList.find(el => el.isactive).roomid
+            this.joinRoom().then(() => resolve())
+          } else {
+            this.addRoom().then(() => this.joinRoom()).then(() => resolve())
+          }
+        })
       })
     },
-    addNum: function () {
-      // eslint-disable-next-line
-      let request = new addNumParams()
-      request.setNumber(Number(this.inputField))
-      // eslint-disable-next-line
-      this.client.addNum(request, {}, (err, response) => {
-        this.inputField = ''
-        this.num = response.toObject()
+    addRoom: async function () {
+      let addRoomParam = new addRoomParams()
+      addRoomParam.setRoomid(this.generateUuid())
+      await new Promise(resolve => {
+        this.client.addRoom(addRoomParam, {}, (err, response) => {
+          const result = response.toObject()
+          this.roomId = result.roomid
+          resolve()
+        })
       })
+    },
+    joinRoom: async function () {
+      let getJoinRoomReq = new joinRoomParams()
+      getJoinRoomReq.setUserid(this.userId)
+      getJoinRoomReq.setRoomid(this.roomId)
+      await new Promise(resolve => {
+        this.client.joinRoom(getJoinRoomReq, {}, (err, response) => {
+          const result = response.toObject()
+          this.ready = result.issuccess
+          resolve()
+        })
+      })
+    },
+    fetchRoomTotalNums: function () {
+      let getRoomTotalNum = new getRoomTotalNumParams()
+      getRoomTotalNum.setRoomid(this.roomId)
+      this.stream = this.client.getRoomTotalNum(getRoomTotalNum)
+      this.stream.on('data', (response) => {
+        let totalnumsList = response.toObject().totalnumsList
+        let userNums = []
+        for (let v in totalnumsList.filter(el => el.roomid === this.roomId)) {
+          userNums.push({'userid': totalnumsList[v].userid, 'player': this.userId == totalnumsList[v].userid,'total': totalnumsList[v].total})
+        }
+        this.start = this.ready && (userNums.length > 1)
+        this.totalnumsList = userNums
+      })
+    },
+    countNum: async function () {
+      let countNumParam = new countNumparams()
+      countNumParam.setUserid(this.userId)
+      countNumParam.setRoomid(this.roomId)
+      await this.client.countNum(countNumParam, {}, (err, response) => {
+        const result = response.toObject()
+        console.log(result)
+      })
+    },
+    generateUuid: function () {
+      let chars = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".split("");
+      for (let i = 0, len = chars.length; i < len; i++) {
+          switch (chars[i]) {
+              case "x":
+                  chars[i] = Math.floor(Math.random() * 16).toString(16);
+                  break;
+              case "y":
+                  chars[i] = (Math.floor(Math.random() * 4) + 8).toString(16);
+                  break;
+          }
+      }
+      return chars.join("");
     }
   }
 }
--- frontend/app/src/count_grpc_web_pb.js
+++ frontend/app/src/count_grpc_web_pb.js
@@ -73,16 +73,16 @@ proto.proto.addNumServicePromiseClient =
 /**
  * @const
  * @type {!grpc.web.MethodDescriptor<
- *   !proto.proto.addNumParams,
+ *   !proto.proto.countNumparams,
  *   !proto.proto.totalNum>}
  */
-const methodDescriptor_addNumService_addNum = new grpc.web.MethodDescriptor(
-  '/proto.addNumService/addNum',
+const methodDescriptor_addNumService_countNum = new grpc.web.MethodDescriptor(
+  '/proto.addNumService/countNum',
   grpc.web.MethodType.UNARY,
-  proto.proto.addNumParams,
+  proto.proto.countNumparams,
   proto.proto.totalNum,
   /**
-   * @param {!proto.proto.addNumParams} request
+   * @param {!proto.proto.countNumparams} request
    * @return {!Uint8Array}
    */
   function(request) {
@@ -93,7 +93,7 @@ const methodDescriptor_addNumService_addNum = new grpc.web.MethodDescriptor(
 
 
 /**
- * @param {!proto.proto.addNumParams} request The
+ * @param {!proto.proto.countNumparams} request The
  *     request proto
  * @param {?Object<string, string>} metadata User defined
  *     call metadata
@@ -102,93 +102,271 @@ const methodDescriptor_addNumService_addNum = new grpc.web.MethodDescriptor(
  * @return {!grpc.web.ClientReadableStream<!proto.proto.totalNum>|undefined}
  *     The XHR Node Readable Stream
  */
-proto.proto.addNumServiceClient.prototype.addNum =
+proto.proto.addNumServiceClient.prototype.countNum =
     function(request, metadata, callback) {
   return this.client_.rpcCall(this.hostname_ +
-      '/proto.addNumService/addNum',
+      '/proto.addNumService/countNum',
       request,
       metadata || {},
-      methodDescriptor_addNumService_addNum,
+      methodDescriptor_addNumService_countNum,
       callback);
 };
 
 
 /**
- * @param {!proto.proto.addNumParams} request The
+ * @param {!proto.proto.countNumparams} request The
  *     request proto
  * @param {?Object<string, string>=} metadata User defined
  *     call metadata
  * @return {!Promise<!proto.proto.totalNum>}
  *     Promise that resolves to the response
  */
-proto.proto.addNumServicePromiseClient.prototype.addNum =
+proto.proto.addNumServicePromiseClient.prototype.countNum =
     function(request, metadata) {
   return this.client_.unaryCall(this.hostname_ +
-      '/proto.addNumService/addNum',
+      '/proto.addNumService/countNum',
       request,
       metadata || {},
-      methodDescriptor_addNumService_addNum);
+      methodDescriptor_addNumService_countNum);
 };
 
 
 /**
  * @const
  * @type {!grpc.web.MethodDescriptor<
- *   !proto.proto.getTotalNumParams,
- *   !proto.proto.totalNum>}
+ *   !proto.proto.getRoomTotalNumParams,
+ *   !proto.proto.roomTotalNums>}
  */
-const methodDescriptor_addNumService_getTotalNum = new grpc.web.MethodDescriptor(
-  '/proto.addNumService/getTotalNum',
-  grpc.web.MethodType.UNARY,
-  proto.proto.getTotalNumParams,
-  proto.proto.totalNum,
+const methodDescriptor_addNumService_getRoomTotalNum = new grpc.web.MethodDescriptor(
+  '/proto.addNumService/getRoomTotalNum',
+  grpc.web.MethodType.SERVER_STREAMING,
+  proto.proto.getRoomTotalNumParams,
+  proto.proto.roomTotalNums,
   /**
-   * @param {!proto.proto.getTotalNumParams} request
+   * @param {!proto.proto.getRoomTotalNumParams} request
    * @return {!Uint8Array}
    */
   function(request) {
     return request.serializeBinary();
   },
-  proto.proto.totalNum.deserializeBinary
+  proto.proto.roomTotalNums.deserializeBinary
 );
 
 
 /**
- * @param {!proto.proto.getTotalNumParams} request The
+ * @param {!proto.proto.getRoomTotalNumParams} request The request proto
+ * @param {?Object<string, string>=} metadata User defined
+ *     call metadata
+ * @return {!grpc.web.ClientReadableStream<!proto.proto.roomTotalNums>}
+ *     The XHR Node Readable Stream
+ */
+proto.proto.addNumServiceClient.prototype.getRoomTotalNum =
+    function(request, metadata) {
+  return this.client_.serverStreaming(this.hostname_ +
+      '/proto.addNumService/getRoomTotalNum',
+      request,
+      metadata || {},
+      methodDescriptor_addNumService_getRoomTotalNum);
+};
+
+
+/**
+ * @param {!proto.proto.getRoomTotalNumParams} request The request proto
+ * @param {?Object<string, string>=} metadata User defined
+ *     call metadata
+ * @return {!grpc.web.ClientReadableStream<!proto.proto.roomTotalNums>}
+ *     The XHR Node Readable Stream
+ */
+proto.proto.addNumServicePromiseClient.prototype.getRoomTotalNum =
+    function(request, metadata) {
+  return this.client_.serverStreaming(this.hostname_ +
+      '/proto.addNumService/getRoomTotalNum',
+      request,
+      metadata || {},
+      methodDescriptor_addNumService_getRoomTotalNum);
+};
+
+
+/**
+ * @const
+ * @type {!grpc.web.MethodDescriptor<
+ *   !proto.proto.addRoomParams,
+ *   !proto.proto.roomInfo>}
+ */
+const methodDescriptor_addNumService_addRoom = new grpc.web.MethodDescriptor(
+  '/proto.addNumService/addRoom',
+  grpc.web.MethodType.UNARY,
+  proto.proto.addRoomParams,
+  proto.proto.roomInfo,
+  /**
+   * @param {!proto.proto.addRoomParams} request
+   * @return {!Uint8Array}
+   */
+  function(request) {
+    return request.serializeBinary();
+  },
+  proto.proto.roomInfo.deserializeBinary
+);
+
+
+/**
+ * @param {!proto.proto.addRoomParams} request The
  *     request proto
  * @param {?Object<string, string>} metadata User defined
  *     call metadata
- * @param {function(?grpc.web.RpcError, ?proto.proto.totalNum)}
+ * @param {function(?grpc.web.RpcError, ?proto.proto.roomInfo)}
  *     callback The callback function(error, response)
- * @return {!grpc.web.ClientReadableStream<!proto.proto.totalNum>|undefined}
+ * @return {!grpc.web.ClientReadableStream<!proto.proto.roomInfo>|undefined}
  *     The XHR Node Readable Stream
  */
-proto.proto.addNumServiceClient.prototype.getTotalNum =
+proto.proto.addNumServiceClient.prototype.addRoom =
     function(request, metadata, callback) {
   return this.client_.rpcCall(this.hostname_ +
-      '/proto.addNumService/getTotalNum',
+      '/proto.addNumService/addRoom',
       request,
       metadata || {},
-      methodDescriptor_addNumService_getTotalNum,
+      methodDescriptor_addNumService_addRoom,
       callback);
 };
 
 
 /**
- * @param {!proto.proto.getTotalNumParams} request The
+ * @param {!proto.proto.addRoomParams} request The
  *     request proto
  * @param {?Object<string, string>=} metadata User defined
  *     call metadata
- * @return {!Promise<!proto.proto.totalNum>}
+ * @return {!Promise<!proto.proto.roomInfo>}
  *     Promise that resolves to the response
  */
-proto.proto.addNumServicePromiseClient.prototype.getTotalNum =
+proto.proto.addNumServicePromiseClient.prototype.addRoom =
     function(request, metadata) {
   return this.client_.unaryCall(this.hostname_ +
-      '/proto.addNumService/getTotalNum',
+      '/proto.addNumService/addRoom',
       request,
       metadata || {},
-      methodDescriptor_addNumService_getTotalNum);
+      methodDescriptor_addNumService_addRoom);
+};
+
+
+/**
+ * @const
+ * @type {!grpc.web.MethodDescriptor<
+ *   !proto.proto.joinRoomParams,
+ *   !proto.proto.joinResult>}
+ */
+const methodDescriptor_addNumService_joinRoom = new grpc.web.MethodDescriptor(
+  '/proto.addNumService/joinRoom',
+  grpc.web.MethodType.UNARY,
+  proto.proto.joinRoomParams,
+  proto.proto.joinResult,
+  /**
+   * @param {!proto.proto.joinRoomParams} request
+   * @return {!Uint8Array}
+   */
+  function(request) {
+    return request.serializeBinary();
+  },
+  proto.proto.joinResult.deserializeBinary
+);
+
+
+/**
+ * @param {!proto.proto.joinRoomParams} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @param {function(?grpc.web.RpcError, ?proto.proto.joinResult)}
+ *     callback The callback function(error, response)
+ * @return {!grpc.web.ClientReadableStream<!proto.proto.joinResult>|undefined}
+ *     The XHR Node Readable Stream
+ */
+proto.proto.addNumServiceClient.prototype.joinRoom =
+    function(request, metadata, callback) {
+  return this.client_.rpcCall(this.hostname_ +
+      '/proto.addNumService/joinRoom',
+      request,
+      metadata || {},
+      methodDescriptor_addNumService_joinRoom,
+      callback);
+};
+
+
+/**
+ * @param {!proto.proto.joinRoomParams} request The
+ *     request proto
+ * @param {?Object<string, string>=} metadata User defined
+ *     call metadata
+ * @return {!Promise<!proto.proto.joinResult>}
+ *     Promise that resolves to the response
+ */
+proto.proto.addNumServicePromiseClient.prototype.joinRoom =
+    function(request, metadata) {
+  return this.client_.unaryCall(this.hostname_ +
+      '/proto.addNumService/joinRoom',
+      request,
+      metadata || {},
+      methodDescriptor_addNumService_joinRoom);
+};
+
+
+/**
+ * @const
+ * @type {!grpc.web.MethodDescriptor<
+ *   !proto.proto.Null,
+ *   !proto.proto.roomList>}
+ */
+const methodDescriptor_addNumService_getRooms = new grpc.web.MethodDescriptor(
+  '/proto.addNumService/getRooms',
+  grpc.web.MethodType.UNARY,
+  proto.proto.Null,
+  proto.proto.roomList,
+  /**
+   * @param {!proto.proto.Null} request
+   * @return {!Uint8Array}
+   */
+  function(request) {
+    return request.serializeBinary();
+  },
+  proto.proto.roomList.deserializeBinary
+);
+
+
+/**
+ * @param {!proto.proto.Null} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @param {function(?grpc.web.RpcError, ?proto.proto.roomList)}
+ *     callback The callback function(error, response)
+ * @return {!grpc.web.ClientReadableStream<!proto.proto.roomList>|undefined}
+ *     The XHR Node Readable Stream
+ */
+proto.proto.addNumServiceClient.prototype.getRooms =
+    function(request, metadata, callback) {
+  return this.client_.rpcCall(this.hostname_ +
+      '/proto.addNumService/getRooms',
+      request,
+      metadata || {},
+      methodDescriptor_addNumService_getRooms,
+      callback);
+};
+
+
+/**
+ * @param {!proto.proto.Null} request The
+ *     request proto
+ * @param {?Object<string, string>=} metadata User defined
+ *     call metadata
+ * @return {!Promise<!proto.proto.roomList>}
+ *     Promise that resolves to the response
+ */
+proto.proto.addNumServicePromiseClient.prototype.getRooms =
+    function(request, metadata) {
+  return this.client_.unaryCall(this.hostname_ +
+      '/proto.addNumService/getRooms',
+      request,
+      metadata || {},
+      methodDescriptor_addNumService_getRooms);
 };
 
 
